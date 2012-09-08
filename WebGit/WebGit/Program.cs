f9047ecaa17c17aa2ec0c584acaa06414643dc97
sinles <hoggins.dev@gmail.com>
2012-09-08 21:55:47 +0300

    add project with git2

---
+++ WebGit/WebGit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibGit2Sharp;
+using System.Globalization;
+
+namespace WebGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var repo = new Repository("D:/repo/test/GitWebClient"))
+            {
+                
+                var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
+
+                foreach (Commit c in repo.Commits.Take(15))
+                {
+                    Console.WriteLine(string.Format("commit {0}", c.Id));
+
+                    if (c.Parents.Count() > 1)
+                    {
+                        Console.WriteLine("Merge: {0}",
+                            string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                    }
+
+                    Console.WriteLine(string.Format("Author: {0} <{1}>", c.Author.Name, c.Author.Email));
+                    Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
+                    Console.WriteLine();
+                    Console.WriteLine(c.Message);
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
